01f7f54fec87850331fe8a2e3518c627c81f6b80
Renza-afro <ratsoane20@gmail.com>
2022-11-08 23:11:40 -0800

    mandatory task

---
+++ sort.h
@@ -0,0 +1,52 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/*Comparison direction macros for bitonic sort*/
+#define UP 0
+#define DOWN 1
+
+/*enum bool - Enumeration of boolean values*/
+
+/**
+* enum bool->............
+* @false: ...
+* @true: ...
+*/
+typedef enum bool
+{
+false = 0,
+true
+} bool;
+
+
+/**
+* struct listint_s - Doubly linked list node
+*
+* @n: Integer stored in the node
+* @prev: Pointer to the previous element of the list
+* @next: Pointer to the next element of the list
+*/
+typedef struct listint_s
+{
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
+} listint_t;
+
+/*Helper for swapping*/
+void swap_ints(int *a, int *b);
+
+/*Printing  functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/*Regular functions prototypes*/
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif
